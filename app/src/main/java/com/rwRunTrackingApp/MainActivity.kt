7130701d3927b9f2761ddce15a72164b13d029fe
Myrick Chow <>
2020-12-12 18:15:38 +0800

    Add MapFragment

--- app/src/main/java/com/rwRunTrackingApp/MainActivity.kt
+++ app/src/main/java/com/rwRunTrackingApp/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.rwRunTrackingApp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Hong Kong and move the camera
+        val latitude = 22.3193
+        val longitude = 114.1694
+        val hongKongLatLong = LatLng(latitude, longitude)
+
+        val zoomLevel = 9.5f
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(hongKongLatLong, zoomLevel))
     }
 }
